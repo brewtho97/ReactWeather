37075d2ba5a2cc1c6a1179000c53745ac23622ea
Thomas Brewer <brewtho@nlg.nhs.uk>
2018-10-17 11:11:36 +0100

    Added heroku request handling

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ const express = require('express');
 
 //Create our app...
 const app = express();
+let port = process.env.PORT || 3000;
+
+app.use(function(request, response, next) {
+	//If the header is found then it moves on otherwise it make the request a
+	//http request.
+	if (request.headers['x-forwarded-proto'] === 'https') {
+		response.redirect('http://' + request.hostname + request.url);
+	} else {
+		next();
+	}
+});
+
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-	console.log('Server started on port 3000');
+
+app.listen(port, function() {
+	console.log(`Server started on port ${port}`);
 });
